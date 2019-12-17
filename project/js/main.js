620bf15a12ce98da187773721e2ed9a969785c80
Shi Jun Neo <sneo@sinmmfyd0bcjk7q.hubspot.local>
2019-12-17 19:11:58 +0800

    seventh try

--- project/js/main.js
+++ project/js/main.js
@@ -9,13 +9,3 @@ function getWeather() {
     console.log(data);
   }})
 }
-
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
-
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
-}
